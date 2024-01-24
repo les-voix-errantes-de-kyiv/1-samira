c41ea01b400637bb941efaf324bc6f0e3d2e27a8
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2024-01-24 14:51:35 +0100

    merge: app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
+
+import * as THREE from 'three'
 import './App.css';
 import Spiral from './Spiral.jsx';
-<<<<<<< HEAD
-import * as THREE from 'three'
-import React, { useEffect, useState, useRef, us } from 'react';
+import Statue from './Statue';
+import React, { useEffect, useState } from 'react';
 import { Canvas, useFrame , useThree} from '@react-three/fiber'
 
 import Card from './Card.jsx';
 import { Image, Environment, ScrollControls, useScroll, useTexture, OrbitControls } from '@react-three/drei';
 import { easing , geometry} from 'maath'
-=======
-import Statue from './Statue';
-import { OrbitControls } from '@react-three/drei';
-import React, { useEffect, useState } from 'react';
->>>>>>> 9bd74b49acbc1656b160ae997c752d939ffaedd6
 
 function App() {
   const [scrollY, setScrollY] = useState(0);
@@ -32,7 +28,7 @@ function App() {
 
   return (
     <div className="canvas-container">
-        <Canvas camera={{ position: [0, 0, 40], fov: 15 }}>
+      <Canvas camera={{ position: [0, 0, 40], fov: 15 }}>
         <ambientLight intensity={1} />
         <spotLight position={[10, 10, 10]} angle={0.45} penumbra={1} decay={0} intensity={2} />
         <pointLight position={[-10, -10, -10]} decay={1} intensity={1} />
