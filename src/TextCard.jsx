2bfdda78ac98f9d4da13920b79e528731cd9c0da
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2024-01-25 16:38:10 +0100

    feat: bg loading fix react build showing white screen

--- src/TextCard.jsx
+++ src/TextCard.jsx
@@ -28,7 +28,7 @@ export default function Card({ url, ...props }) {
 
   return (
       <Image ref={ref} url={url} side={THREE.DoubleSide} {...materialProps} {...props}>
-        <bentPlaneGeometry args={[0.00000001, 2, 1.2, 40, 20]} />
+        <bentPlaneGeometry args={[0.0000001, 2, 1.2, 40, 20]} />
       </Image>      
   );
 }
