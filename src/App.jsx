dbaa61cd9eb3eb71d13d181c6934b55374bfe193
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2024-01-25 10:24:52 +0100

    feat: spin of cards like spiral

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import { easing , geometry} from 'maath'
 import Statue from './Statue';
 import Spiral from './Spiral.jsx';
 
+let sense = 1; // 1 or -1
+let senseZ = -1; // 1 or -1
+let loopPoint = 1;
 
 function App() {
   const [scrollY, setScrollY] = useState(0);
@@ -35,7 +38,7 @@ function App() {
         <pointLight position={[-10, -10, -10]} decay={1} intensity={1} />
         <ScrollControls pages={8} >
           <Rig rotation={[0, Math.PI, 0]}>
-            <Spiral rotation={[0, Math.PI  , 0] }position={[0, 0, 0]} scale={[0.055, 0.055, 0.055]} scrollY={scrollY}  />
+            {/*<Spiral rotation={[0, Math.PI  , 0] }position={[0, 0, 0]} scale={[0.055, 0.055, 0.055]} scrollY={scrollY}  />*/}
             <Cards />
           </Rig>
           <Axis rotation={[0, 0, 0]}>
@@ -44,7 +47,7 @@ function App() {
           <gridHelper args={[10, 10]} />
         </ScrollControls>
         
-        {/* <OrbitControls /> */}
+        { /*<OrbitControls />*/ }
       </Canvas>
     </div>
   );
@@ -78,29 +81,49 @@ function Cards() {
   const numberOfCards = 10;
   var objPerTurn = 2;
   let lastPosition = 0;
-  let sense = 1; // 1 or -1
-  const cardPosition = (i, lastPosition, sense) => {
-    const step = 2;
+
+  const cardPosition = (i, lastPosition, sense, senseZ) => {
+    const step = 1.3;
     const position = new THREE.Vector3();
-    const radius = 1; // radius of the spiral
+    const radius = 1.5; // radius of the spiral
 
     // let angleStep = (Math.PI * 4) / objPerTurn;
 
     // position.x = (Math.cos(angleStep * i) * radius ) * 0.1 - .8;
     // position.y = lastPosition + Math.sin(angleStep * i) * radius * 0.1;
     // position.z = (Math.sin(angleStep * i) * radius) / (Math.PI * 2) * -0.1;
-    position.x =  radius * sense ;
-    position.y = -(i * step + 1) + 2.5// y is always negative
-    position.z = radius * sense;
+
+    if(loopPoint == 1){
+
+      position.x =  radius * sense ;
+      position.z = 0;
+      position.rotationY = Math.PI/2 + Math.PI
+
+    }else if (loopPoint == -1){
+
+      position.x =  0 ;
+      position.z = radius * senseZ ;
+      position.rotationY = Math.PI*2 + Math.PI
+
+    }
+
+    position.y = -(i * step + 1) + 2.5// y is always negative (decreasing)
+
+
     // console.log("position: " + i, position);
     return position;
   };
 
-  return Array.from({ length: numberOfCards, sense }, (_, i) => {
-    const position = cardPosition(i, lastPosition, sense); // Call the function to get the position
+  return Array.from({ length: numberOfCards, sense, senseZ }, (_, i) => {
+
+    if(loopPoint == 1){ sense *= -1; }else if (loopPoint == -1){ senseZ *= -1; }
+    loopPoint *= -1;
+
+    console.log("sense "+sense)
+
+    const position = cardPosition(i, lastPosition, sense, senseZ); // Call the function to get the position
     lastPosition = position.y;
     console.log("Card Position: " + i, position);
-    sense *= -1;
 
     return (
       <Card
@@ -108,7 +131,7 @@ function Cards() {
         url={`/img${Math.floor(i % 10) + 1}_.jpg`}
         position={[position.x, position.y, position.z]}
         text={`Card ${i}`}
-        rotation={[0,  (Math.PI *2), 0]}
+        rotation={[0,  position.rotationY, 0]}
       />
     );
   });
